eafc18fa101b181498c6363af84cfee7af4b927b
Midhilesh <72137078+Midhilesh-19@users.noreply.github.com>
2020-12-25 20:44:08 -0800

    Add files via upload

---
+++ ph.py
@@ -0,0 +1,81 @@
+print('run')
+name="MIKE"
+age=19
+Gf="selena"
+isHot=True
+isRich=False
+print(name)
+print(age)
+print(27+44)
+print(160%3)
+
+#ADD OF TWO VALUES
+spider =5
+ironman =50
+print(spider+ironman)
+
+#COMBINING TWO NAMES
+firstname="Justin"
+lastname="Hooker"
+print( firstname +" "+lastname )
+
+#INPUT STATEMENT
+Name=input("Provide ur Name:")
+print(Name)
+
+Age=input("Provide ur Age:")
+print(Age)
+#TWO INPUT SUM
+num1 = input("enter first num:")
+num2 = input("enter second num:")
+n1=int(num1)
+n2=int(num2)
+print(n1+n2)
+
+
+num1 = input("enter first num:")
+num2 = input("enter second num:")
+n1=float(num1)
+n2=float(num2)
+print(n1+n2)
+
+#conditional statements
+price=700
+
+if price>600:
+    print(" I wont buy it")
+else:
+    print("I will give a gift") 
+
+
+price=500
+
+if price>600:
+    print(" I wont buy it")
+else:
+    print("I will give a gift") 
+          
+
+#for loop
+ages = [13,17,19,50]
+
+
+for n in ages:
+    print("this friend ",n)
+
+
+for num in range (1,10): 
+    print(num)   
+
+#while loop
+n=0
+while n < 13:
+    print(n)
+    n+=1
+
+#random number for ex:die
+import random
+n = random.randint(0,10)
+print(n)
+
+
